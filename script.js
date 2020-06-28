9246a9ffe4eacfa4dcf251b8afd9e2c690fdeaf7
Aidan Sweeny <apswee22@colby.edu>
2020-06-28 14:52:48 -0400

    Added functionalityto city buttons

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-$("#searchBtn").on("click", function(event) {
-    var city = $("#citySearch").val();
-    var button = $("<button>");
-    button.text(city);
-    button.attr("class", "cityBtn");
-    console.log(city)
-    $("#cities").prepend(button);
-})
-$(".cityBtn").on("click", function(event) {
-    var city = event.val()
-    console.
-})
 $(document).ready(function() {
-    var APIKey = "166a433c57516f51dfab1f7edaed8413";
-    var city = "atlanta";
-    // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    });
+    $("#searchBtn").on("click", function(event) {
+        var city = $("#citySearch").val();
+        var button = $("<button>");
+        button.text(city);
+        button.attr("class", "cityBtn");
+        console.log(city)
+        $("#cities").prepend(button);
+    })
+    $("#cities").on("click", function(event) {
+        var city = event.target.textContent
+        var APIKey = "166a433c57516f51dfab1f7edaed8413";
+        // Here we are building the URL we need to query the database
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        });
+    })
 });
