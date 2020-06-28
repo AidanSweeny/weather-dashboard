ac3bb845ec1648a7d7ae481b962ccf67d26c1637
Aidan Sweeny <apswee22@colby.edu>
2020-06-28 14:47:51 -0400

    Created button list

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+$("#searchBtn").on("click", function(event) {
+    var city = $("#citySearch").val();
+    var button = $("<button>");
+    button.text(city);
+    button.attr("class", "cityBtn");
+    console.log(city)
+    $("#cities").prepend(button);
+})
+$(".cityBtn").on("click", function(event) {
+    var city = event.val()
+    console.
+})
 $(document).ready(function() {
-    queryURL = "htpps://api.openweathermap.org/data/2.5/weather?q=SanFrancisco,California&appid=8e279ea4e87ca0ed65b17053a29ece11"
+    var APIKey = "166a433c57516f51dfab1f7edaed8413";
+    var city = "atlanta";
+    // Here we are building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
     $.ajax({
         url: queryURL,
         method: "GET"
